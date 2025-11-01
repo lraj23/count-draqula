756778126f04490b58dee548d60c27ee770fe5ca
Lavith Raj <189830163+lraj23@users.noreply.github.com>
2025-11-01 14:39:00 -0700

    allow bot to understand messages sent rapidly

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ import app from "./client.js";
 import { getCDraqula, saveState } from "./datahandler.js";
 const lraj23UserId = "U0947SL6AKB";
 const lraj23BotTestingId = "C09GR27104V";
-const msgIsNum = msg => parseInt(msg.split(" - ")[0]).toString() === msg.split(" - ")[0];
+const msgIsNum = msg => (msg ? parseInt(msg.split(" - ")[0]).toString() === msg.split(" - ")[0] : false);
+const numInMsg = msg => msgIsNum(msg) ? parseInt(msg.split(" - ")[0]) : NaN;
 
-app.message("", async ({ message: { text, channel, ts } }) => {
+app.message("", async ({ message: { text, user, channel, ts } }) => {
 	if (channel !== lraj23BotTestingId) return;
 	let CDraqula = getCDraqula();
-	const react = async (name, timestamp) => await app.client.reactions.add({ channel, name, timestamp });
+	const react = async (name, timestamp) => {
+		try {
+			await app.client.reactions.add({ channel, name, timestamp });
+		} catch (e) {
+			console.error(e.data.error);
+		}
+	};
+	const unreact = async (name, timestamp) => {
+		try {
+			await app.client.reactions.remove({ channel, name, timestamp });
+		} catch (e) {
+			console.error(e.data.error);
+		}
+	};
 	if (!msgIsNum(text)) {
 		console.log("not a number!");
 		return await react("very-mad", ts);
@@ -19,7 +33,38 @@ app.message("", async ({ message: { text, channel, ts } }) => {
 		await react("white_check_mark", ts);
 	} else {
 		console.log("wrong...");
-		await react("bangbang", ts);
+		let pastMessages = (await app.client.conversations.history({
+			channel,
+			latest: ts,
+			oldest: ts - 20000,
+			limit: 50
+		})).messages.filter(msg => msgIsNum(msg.text));
+		let isCorrect = [true];
+		for (let i = 0; i < pastMessages.length; i++) {
+			if (numInMsg(pastMessages[i].text) + 1 === CDraqula.next) {
+				isCorrect[1] = i;
+				break;
+			}
+			if (numInMsg(pastMessages[i].text) + 1 !== counted - i) {
+				isCorrect[0] = false;
+				break;
+			}
+		}
+		if (counted !== CDraqula.next + isCorrect[1]) isCorrect = [false];
+		if (isCorrect[0]) {
+			for (let i = 0; i < isCorrect[i]; i++) {
+				let msg = pastMessages[i];
+				if (i > isCorrect[1]) return;
+				await react("white_check_mark", msg.ts);
+				await unreact("bangbang", msg.ts);
+				CDraqula.next++;
+			}
+			await react("white_check_mark", ts);
+			CDraqula.next++;
+		} else {
+			await react("bangbang", ts);
+			await app.client.chat.postEphemeral({ channel, user, text: "That's the wrong number... It should be " + CDraqula.next });
+		}
 	}
 	console.log(counted, CDraqula.next);
 	saveState(CDraqula);
